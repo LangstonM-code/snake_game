471a023f002a09428e22e37cdb4ff2346738f137
Langston McKee <langston.mckee@mykenzie.snhu.edu>
2022-01-23 07:21:18 -0500

    Added all methods and classes

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -1,13 +1,43 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class GamePanel extends JPanel implements ActionListener {
-    GamePanel(){
+    GamePanel() {
+
+    }
+    public void startGame() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) {
+
+    }
+    public void move() {
+
+    }
+    public void checkFruit(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
 
     }
     @Override
     public void actionPerformed (ActionEvent e) {
         //TODO Complete Method
+    }
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
     }
 }
